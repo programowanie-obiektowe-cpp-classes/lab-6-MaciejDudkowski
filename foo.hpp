7b038b50eb6f1543c52a7a6364db404dc31fb9b1
Maciej Dudkowski <01170746@pw.edu.pl>
2025-11-30 18:33:27 +0100

    zejściówka lab 6

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,12 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& h) { h.birthday(); });
+
+    std::transform(people.rbegin(), people.rend(), std::back_inserter(retval), [](const Human& h) {
+        return h.isMonster() ? 'n' : 'y';
+    });
+
 
     return retval;
 }
